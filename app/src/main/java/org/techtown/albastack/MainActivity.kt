ba760df9a3f99896637808f55a8d08fed0a615d6
kkmarron <aoyujeong@naver.com>
2023-11-27 22:56:53 +0900

    SplashScreen, Toolbar 생성

---
+++ app/src/main/java/org/techtown/albastack/MainActivity.kt
@@ -0,0 +1,17 @@
+package org.techtown.albastack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+        supportActionBar!!.setDisplayShowTitleEnabled(false)
+    }
+}
